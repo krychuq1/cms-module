ec18bd35e10d52e9026bee638877cf15b91b494c
Unknown <krys.nagorny@gmail.com>
2019-05-13 20:30:12 +0200

    add dist

---
+++ dist/src/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Cms = /** @class */ (function () {
+    function Cms() {
+    }
+    Cms.prototype.printMe = function () {
+        console.log('test');
+    };
+    return Cms;
+}());
+exports.default = new Cms();
