55e2a689712debbcd276e8b327b0c0dc0b5e6558
Unknown <krys.nagorny@gmail.com>
2019-05-15 11:55:19 +0200

    last build

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,11 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-class Cms {
-    constructor() {
+var axios_1 = __importDefault(require("axios"));
+var Cms = /** @class */ (function () {
+    function Cms(projectId) {
+        this.projectId = projectId;
+        this.url = 'https://translation-backend.sevenamstudio.com/api/content/' + projectId + '/';
     }
-    printMe() {
-        console.log('test');
-    }
-}
-const cmsObj = new Cms();
-exports.default = cmsObj;
+    Cms.prototype.getContent = function (type, name, language) {
+        var url = this.url + type + '/' + name + '/' + language;
+        return axios_1.default.get(url).then(function (data) {
+            return data;
+        }, function (error) {
+            return error;
+        });
+    };
+    return Cms;
+}());
+exports.Cms = Cms;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
 class Cms {
-  constructor() {
-    
-  }
-  printMe(): void {
-    console.log('test');
-  }
+    public projectId: string;
+    public url: string;
+
+    public constructor(projectId: string){
+        this.projectId = projectId;
+        this.url = 'https://translation-backend.sevenamstudio.com/api/content/' + projectId + '/'
+    }
+    public getContent(type: string, name: string, language: string): Promise<AxiosResponse<object[]> | AxiosError> {
+        let url = this.url + type + '/' + name + '/' + language;
+        return axios.get <object[]> (url).then((data)=>{
+            return data;
+        }, (error): AxiosError => {
+            return error;
+        })
+    }
+
 }
-const cmsObj = new Cms();
-export default cmsObj;
+// console.log("test");
+// const cmsObj = new Cms('5cc969d652f93943b29f8bd2');
+// cmsObj.getContent('component', 'header', 'Polish').then((data)=>{
+//   console.log(data);
+// }, (error): AxiosError => {
+//   console.log(error);
+// });
+
+export {Cms};
